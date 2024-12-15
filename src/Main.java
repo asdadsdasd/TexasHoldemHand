b028bef24f5db8805e70f6c388884f5a48b35a51
asdadsdasd <mkozarez12345@mail.ru>
2024-12-15 20:16:31 +0300

    Completed second point of task. Not tested yet

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,27 @@
 import entities.PokerHand;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<PokerHand> hands = new ArrayList<PokerHand>();
-        {
-            hands.add(new PokerHand("KS 2H 5C JD TD"));
-            hands.add(new PokerHand("2S 2H 2C 2D AD"));
-            hands.add(new PokerHand("AH 2H 3H 4H 5H"));
-            hands.add(new PokerHand("5H 4H 3H 2H AH"));
-        }
-        for(PokerHand hand : hands){
-            System.out.println(hand);
+        ArrayList<PokerHand> hands = new ArrayList<>();
+        hands.add(new PokerHand("5H 6H 7H 8H 9H")); // Стрит флеш
+        hands.add(new PokerHand("2H 2C 2S 2D 5C")); // Каре
+        hands.add(new PokerHand("2H 2C 2S 3D 3C")); // Фулл хаус
+        hands.add(new PokerHand("2H 3H 5H 7H 9H")); // Флеш
+        hands.add(new PokerHand("2H 3C 4D 5S 6H")); // Стрит
+        hands.add(new PokerHand("2H 2C 2S JD TD")); // Сет
+        hands.add(new PokerHand("2H 2C 5C 5D TD")); // Две пары
+        hands.add(new PokerHand("2H 2C 5C JD TD")); // Пара
+        hands.add(new PokerHand("KS 2H 5C JD TD")); // Рука с высоким карманом
+
+        Collections.sort(hands);
+
+        System.out.println("Sorted Hands:");
+        for (PokerHand hand : hands) {
+            System.out.println(hand + "   Rank: " + hand.getCombinationRank() + "   Power: " + hand.getCombinationPower());
         }
     }
 }
--- src/entities/PokerHand.java
+++ src/entities/PokerHand.java
@@ -1,14 +1,18 @@
 package entities;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import entities.enums.Rank;
+import entities.enums.Suit;
 
-public class PokerHand {
+import java.util.*;
+
+public class PokerHand implements Comparable<PokerHand> {
     private final List<Card> cards;
+    private int combinationRank;   // Сила комбинации
+    private int combinationPower;  // Сила внутри одной комбинации
+
 
     public PokerHand(String hand) {
-        if (hand.isBlank() || hand.isEmpty()) {
+        if (hand == null|| hand.isBlank()) {
             throw new IllegalArgumentException("Строка с картами не должна быть пустой");
         }
 
@@ -23,6 +27,224 @@ public class PokerHand {
         }
 
         Collections.sort(cards);
+        evaluateHand();
+    }
+
+    public int getCombinationRank() {
+        return combinationRank;
+    }
+
+    public int getCombinationPower() {
+        return combinationPower;
+    }
+
+    private boolean checkForStraightFlush() {
+        if (checkForFlush() && checkForStraight()) {
+            combinationRank = 8;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkForFourOfKind() {
+        Rank fourOfKindRank = getRankByCount(4);
+        Rank kickerRank = getRankByCount(1);
+
+        if (fourOfKindRank != null && kickerRank != null) {
+            combinationRank = 7;
+            combinationPower = Integer.parseInt(fourOfKindRank.getValue() + String.format("%02d", kickerRank.getValue()));
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkForFullHouse() {
+        Rank threeOfKindRank = getRankByCount(3);
+        Rank pairRank = getRankByCount(2);
+
+        if (threeOfKindRank != null && pairRank != null) {
+            combinationRank = 6;
+            combinationPower = Integer.parseInt(threeOfKindRank.getValue() + String.format("%02d", pairRank.getValue()));
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkForFlush() {
+        Suit firstCardSuit = cards.get(0).getSuit();
+        for (Card card : cards) {
+            if(card.getSuit().getSymbol() != firstCardSuit.getSymbol()) {
+                return false;
+            }
+        }
+        combinationRank = 5;
+        StringBuilder combinationPowerString = new StringBuilder();
+        for (int i = cards.size() - 1; i >= 0; i--) {
+            combinationPowerString.append(String.format("%02d", cards.get(i).getRank().getValue()));
+        }
+        combinationPower = Integer.parseInt(combinationPowerString.toString());
+        return true;
+    }
+
+    private boolean checkForStraight() {
+        boolean isSequential = true;
+        for (int i = 1; i < cards.size(); i++) {
+            if (cards.get(i).getRank().getValue() != cards.get(i - 1).getRank().getValue() + 1) {
+                isSequential = false;
+                break;
+            }
+        }
+
+        boolean isAceLowStraight = false;
+        if (!isSequential) {
+            isAceLowStraight = cards.get(4).getRank() == Rank.ACE &&
+                    cards.get(0).getRank() == Rank.TWO &&
+                    cards.get(1).getRank() == Rank.THREE &&
+                    cards.get(2).getRank() == Rank.FOUR &&
+                    cards.get(3).getRank() == Rank.FIVE;
+        }
+
+        if (isSequential || isAceLowStraight) {
+            combinationRank = 4;
+            if (isAceLowStraight) {
+                combinationPower = cards.get(3).getRank().getValue();
+            }
+            else {
+                combinationPower = cards.get(4).getRank().getValue();;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkForThreeOfKind() {
+        Rank threeOfKindRank = getRankByCount(3);
+
+        if (threeOfKindRank != null) {
+            combinationRank = 3;
+            StringBuilder combinationPowerString = new StringBuilder(threeOfKindRank.getValue());
+            combinationPowerString.append(String.format("%d", threeOfKindRank.getValue()));
+            for (int i = cards.size() - 1; i >= 0; i--) {
+                if (cards.get(i).getRank().getValue() != threeOfKindRank.getValue()) {
+                    combinationPowerString.append(String.format("%02d", cards.get(i).getRank().getValue()));
+                }
+            }
+            combinationPower = Integer.parseInt(combinationPowerString.toString());
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkForTwoPair() {
+        Map<Rank, Integer> rankCounts = new HashMap<>();
+
+        for (Card card : cards) {
+            rankCounts.put(card.getRank(), rankCounts.getOrDefault(card.getRank(), 0) + 1);
+        }
+
+        List<Rank> listOfPairs = new ArrayList<>();
+        Rank singlePair = null;
+        for (Map.Entry<Rank, Integer> entry : rankCounts.entrySet()) {
+            if (entry.getValue() == 2) {
+                listOfPairs.add(entry.getKey());
+            }
+            else {
+                singlePair = entry.getKey();
+            }
+        }
+        StringBuilder combinationPowerString = new StringBuilder();
+        if (listOfPairs.size() == 2 && singlePair != null) {
+            if (listOfPairs.get(0).getValue() > listOfPairs.get(1).getValue()) {
+                combinationPowerString.append(listOfPairs.get(0).getValue()).
+                        append(String.format("%02d", cards.get(1).getRank().getValue())).
+                        append(String.format("%02d", singlePair.getValue()));
+
+            }
+            else {
+                combinationPowerString.append(listOfPairs.get(1).getValue()).
+                        append(String.format("%02d", cards.get(0).getRank().getValue())).
+                        append(String.format("%02d", singlePair.getValue()));
+            }
+            combinationRank = 2;
+            combinationPower = Integer.parseInt(combinationPowerString.toString());
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkForOnePair() {
+        Map<Rank, Integer> rankCounts = getRankCounts();
+        if (rankCounts.size() != 4 && !rankCounts.containsValue(2)) {
+            return false;
+        }
+        Rank pairRank = getRankByCount(2);
+        StringBuilder combinationPowerString = new StringBuilder();
+        combinationPowerString.append(String.format("%d", pairRank.getValue()));
+        for (int i = cards.size() - 1; i >= 0; i--) {
+            if (cards.get(i).getRank().getValue() != pairRank.getValue()) {
+                combinationPowerString.append(String.format("%02d", cards.get(i).getRank().getValue()));
+            }
+        }
+
+        combinationRank = 1;
+        combinationPower = Integer.parseInt(combinationPowerString.toString());
+        return true;
+    }
+
+    private boolean checkForHighCards() {
+        Map<Rank, Integer> rankCounts = getRankCounts();
+        if (rankCounts.size() != 5) {
+            return false;
+        }
+        StringBuilder combinationPowerString = new StringBuilder();
+        combinationPowerString.append(String.format("%d", cards.get(4).getRank().getValue()));
+        for (int i = cards.size() - 2; i >= 0; i--) {
+            combinationPowerString.append(String.format("%02d", cards.get(i).getRank().getValue()));
+        }
+        combinationRank = 0;
+        combinationPower = Integer.parseInt(combinationPowerString.toString());
+        return true;
+    }
+
+    private void evaluateHand() {
+        if (checkForStraightFlush()) return;
+        if (checkForFourOfKind()) return;
+        if (checkForFullHouse()) return;
+        if (checkForFlush()) return;
+        if (checkForStraight()) return;
+        if (checkForThreeOfKind()) return;
+        if (checkForTwoPair()) return;
+        if (checkForOnePair()) return;
+        checkForHighCards();
+    }
+
+    private Rank getRankByCount(int count) {
+        Map<Rank, Integer> rankCounts = getRankCounts();
+
+        return rankCounts.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() == count)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+    }
+
+    private Map<Rank, Integer> getRankCounts() {
+        Map<Rank, Integer> rankCounts = new HashMap<>();
+        for (Card card : cards) {
+            rankCounts.put(card.getRank(), rankCounts.getOrDefault(card.getRank(), 0) + 1);
+        }
+        return rankCounts;
+    }
+
+    @Override
+    public int compareTo(PokerHand other) {
+        int rankComparison = Integer.compare(other.combinationRank, this.combinationRank);
+        if (rankComparison != 0) {
+            return rankComparison;
+        }
+
+        return Integer.compare(other.combinationPower, this.combinationPower);
     }
 
     @Override
@@ -31,6 +253,6 @@ public class PokerHand {
         for (Card card : cards) {
             sb.append(card).append(" ");
         }
-        return sb.toString();
+        return sb.toString().trim();
     }
 }
